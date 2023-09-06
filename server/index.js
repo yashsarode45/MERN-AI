00d0d3e5ff7c0ff75e29adda7c615f34a026cde9
Yash Sarode <yashsarode45@gmail.com>
2023-09-06 23:23:35 +0530

    CORS change

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors({
-  origin:"http://localhost:3000",
+  origin:"https://mern-ai-mauve.vercel.app",
   credentials:true,
 }));
 app.use(express.json({limit: '50mb'}))
