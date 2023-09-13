8de4f00dac0590ab61186980bee0167c4a9bc3a4
Yash Sarode <yashsarode45@gmail.com>
2023-09-14 00:31:32 +0530

    CORS Update

--- server/index.js
+++ server/index.js
@@ -11,9 +11,10 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors({
-  origin:"https://mern-ai-mauve.vercel.app",
+  origin:"*",
   credentials:true,
 }));
+
 app.use(express.json({limit: '50mb'}))
 app.use(
 	fileUpload({
